0bec1c1295148d76f31597ab8199ed4fde7e1990
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-03-24 14:38:29 -0600

    Agrega la clase paciente asegurado

---
+++ paciente-asegurado.js
@@ -0,0 +1,22 @@
+import Paciente from "./paciente.js"
+import Fecha from "./fecha.js"
+export default class PacienteAsegurado extends Paciente{
+    /**
+     * 
+     * @param {number} numeroPoliza 
+     * @param {Fecha} fechaFinVigencia 
+     * @param {string} compañia 
+     */
+    constructor(numeroPoliza, fechaFinVigencia, compañia){
+        super(nombre, fechaNacimiento, telefono)
+        this._numeroPoliza = numeroPoliza
+        this._fechaFinVigencia = fechaFinVigencia
+        this._compañia = compañia
+        this._nombre = nombre
+        this._fechaNacimiento = fechaNacimiento
+        this._telefono = telefono
+    }
+    getPerfil(){
+        return(`${this._nombre.getNombreCompleto()}, ${this._fechaNacimiento.getFecha()}, ${this._telefono}, ${this._numeroPoliza}, ${this._fechaFinVigencia}, ${this.compañia}`)
+    }
+}
